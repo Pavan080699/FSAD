c6e6baa34f26e7399863bb0a08318d474053ce50
Pavan080699 <pavanteja2006august@gmail.com>
2026-03-04 09:44:02 +0530

    Add files via upload

---
+++ SpringBoot_Demo Skill-5/src/main/java/klu/SpringBoot_Demo/SpringBootDemoApplication.java
@@ -0,0 +1,20 @@
+package klu.SpringBoot_Demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDemoApplication.class, args);
+		
+		System.out.println("Welcome");
+		System.out.println("Welcome to spring boot");
+		System.out.println("Welcome to spring boot application");
+		System.out.println("Welcome to spring boot sec-15");
+
+
+	}
+
+}
